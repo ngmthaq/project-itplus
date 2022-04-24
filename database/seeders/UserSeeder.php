3a8e12fbe8ea0b67de0bf967d953eeb34bf1f64b
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-04-24 14:16:56 +0700

    new seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -26,5 +26,15 @@ class UserSeeder extends Seeder
             'remember_token' => md5('NguyễnThắngnguyenmanhthang2000.fb@gmail.com' . date('Y-m-d H:i:s')),
             'token' => md5('NguyễnThắngnguyenmanhthang2000.fb@gmail.com' . date('Y-m-d H:i:s')),
         ]);
+        User::create([
+            'role_id' => 1,
+            'first_name' => 'Nguyễn',
+            'last_name' => 'Thắng',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('thang2000'), // thang2000
+            'remember_token' => md5('NguyễnThắngnguyenmanhthang2000.fb@gmail.com' . date('Y-m-d H:i:s')),
+            'token' => md5('NguyễnThắngnguyenmanhthang2000.fb@gmail.com' . date('Y-m-d H:i:s')),
+        ]);
     }
 }
